4173736e59abce82c9357d92820a162581cd141a
Nicholas Bevans <bevani01@8263.lc.luther.edu>
2018-03-06 08:09:23 -0600

    most recent

--- js/shopping.js
+++ js/shopping.js
@@ -1,18 +1,26 @@
 
 function  clickedon() {
     let name = document.querySelector("#nameitem").value;
-    let quantity = document.querySelector("#quantityitems").value
+    let quantity = document.querySelector("#quantityitems").value;
     let priority = document.querySelector("#myPriority").value;
     let section = document.querySelector("#mySection").value
     let store = document.querySelector("#myStore").value;
-    let price = document.querySelector("#priceitem").value
-    let mytable = document.querySelector("#shoplist").value
-    mytable.innerHTML = "";
-    lst = [name, quantity, priority, store, section, price]; 
+    let price = document.querySelector("#priceitem").value;
+    let mytable = document.querySelector("#shoplist")
+    lst = [name, quantity, priority, store, section, price];
+    let row = document.createElement("tr")
     for (p in lst) {
         let cell = document.createElement("td")
-        cell.innerHTML = lst(p)
+        cell.innerHTML = lst[p]
         row.appendChild(cell)
     }
     mytable.appendChild(row)
+}
+
+function item() {
+    let name = document.querySelector("#nameitem").value;
+}
+
+function shoplist() {
+    let mytable = document.querySelector("#shoplist")
 }

97cb0801a797af52f7347b669d21e98044930137
Nicholas Bevans <bevani01@8263.lc.luther.edu>
2018-03-22 09:08:39 -0500

    most recent

--- shop.py
+++ shop.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route(`/shop`)
+@app.route('/shop')
 def shop():
 	lst1 = request.args['lst1']
 	lst2 = request.args['lst2']
